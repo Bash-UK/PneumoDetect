8812e17cb2ab975f37d4c5004013fb1c6c2d8e55
BashUK <umairahmaduk173@gmail.com>
2025-07-29 14:35:37 +0530

    changing endpoint to render.com

--- frontend/pneumonia-frontend/src/App.js
+++ frontend/pneumonia-frontend/src/App.js
@@ -71,7 +71,7 @@ export default function App() {
     try {
       const formData = new FormData();
       formData.append("file", file);
-      const res = await fetch("http://127.0.0.1:8000/predict/", {
+      const res = await fetch("https://pneumodetectapp.onrender.com/predict/", {
         method: "POST",
         body: formData
       });
